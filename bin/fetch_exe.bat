ffcd66d53217e0526ff33470406785b1e0b9f98e
Chris Toshok <ctoshok@replay.io>
2024-02-04 08:17:44 -0700

    remove the double quotes maybe?  also echo the url we're fetching from

--- bin/fetch_exe.bat
+++ bin/fetch_exe.bat
@@ -1,4 +1,4 @@
-::@echo off
+@echo off
 SETLOCAL
 
 SET "BUILDEVENTS_GH_ORG=replayio"
@@ -10,8 +10,9 @@ SET "ARCH=amd64"
 SET "PLUGIN_BIN=%~dp0"
 
 :: Download buildevents if not exists
-IF NOT EXIST "%PLUGIN_BIN%buildevents.exe" (
-    curl -Lf -o "%PLUGIN_BIN%buildevents.exe" "https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/%BUILDEVENTS_RELEASE%/buildevents-windows-%ARCH%.exe"
+IF NOT EXIST %PLUGIN_BIN%buildevents.exe (
+    echo Downloading buildevents from url: https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/%BUILDEVENTS_RELEASE%/buildevents-windows-%ARCH%.exe
+    curl -Lf -o %PLUGIN_BIN%buildevents.exe https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/%BUILDEVENTS_RELEASE%/buildevents-windows-%ARCH%.exe
 )
 
 ENDLOCAL
