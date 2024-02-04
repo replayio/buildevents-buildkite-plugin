a62644a83066cb90e9b86501c25c6bdaf4528f48
Chris Toshok <ctoshok@replay.io>
2024-02-04 09:16:25 -0700

    stop trying to url escape the /

--- bin/fetch_exe.bat
+++ bin/fetch_exe.bat
@@ -2,7 +2,7 @@
 SETLOCAL
 
 SET "BUILDEVENTS_GH_ORG=replayio"
-SET "BUILDEVENTS_RELEASE=toshok^^%2Fadd-buildkite-watch-v0.3"
+SET "BUILDEVENTS_RELEASE=toshok/add-buildkite-watch-v0.3"
 
 :: For now, assume amd64.  is there a way to detect this?
 SET "ARCH=amd64"
@@ -11,10 +11,8 @@ SET "PLUGIN_BIN=%~dp0"
 
 :: Download buildevents if not exists
 IF NOT EXIST %PLUGIN_BIN%buildevents.exe (
-::    echo Downloading buildevents from url: https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/%BUILDEVENTS_RELEASE%/buildevents-windows-%ARCH%.exe
-::    curl -Lf -o %PLUGIN_BIN%buildevents.exe https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/%BUILDEVENTS_RELEASE%/buildevents-windows-%ARCH%.exe
-    echo Downloading buildevents from url: https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/toshok^%2Fadd-buildkite-watch-v0.3/buildevents-windows-%ARCH%.exe
-    curl -Lf -o %PLUGIN_BIN%buildevents.exe https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/toshok^%2Fadd-buildkite-watch-v0.3/buildevents-windows-%ARCH%.exe
+    echo Downloading buildevents from url: https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/%BUILDEVENTS_RELEASE%/buildevents-windows-%ARCH%.exe
+    curl -Lf -o %PLUGIN_BIN%buildevents.exe https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/%BUILDEVENTS_RELEASE%/buildevents-windows-%ARCH%.exe
 )
 
 ENDLOCAL
