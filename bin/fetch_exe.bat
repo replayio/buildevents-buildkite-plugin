28cfb146dc7c81cf58542c8fc12d28c39f4d8aef
Chris Toshok <ctoshok@replay.io>
2024-02-04 08:53:34 -0700

    major dogscience

--- bin/fetch_exe.bat
+++ bin/fetch_exe.bat
@@ -1,8 +1,8 @@
 @echo off
-SETLOCAL enabledelayedexpansion
+SETLOCAL
 
 SET "BUILDEVENTS_GH_ORG=replayio"
-SET "BUILDEVENTS_RELEASE=toshok^%2Fadd-buildkite-watch-v0.3"
+SET "BUILDEVENTS_RELEASE=toshok^^%2Fadd-buildkite-watch-v0.3"
 
 :: For now, assume amd64.  is there a way to detect this?
 SET "ARCH=amd64"
@@ -11,8 +11,8 @@ SET "PLUGIN_BIN=%~dp0"
 
 :: Download buildevents if not exists
 IF NOT EXIST %PLUGIN_BIN%buildevents.exe (
-    echo Downloading buildevents from url: https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/!BUILDEVENTS_RELEASE!/buildevents-windows-%ARCH%.exe
-    curl -Lf -o %PLUGIN_BIN%buildevents.exe https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/!BUILDEVENTS_RELEASE!/buildevents-windows-%ARCH%.exe
+    echo Downloading buildevents from url: https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/%BUILDEVENTS_RELEASE%/buildevents-windows-%ARCH%.exe
+    curl -Lf -o %PLUGIN_BIN%buildevents.exe https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/%BUILDEVENTS_RELEASE%/buildevents-windows-%ARCH%.exe
 )
 
 ENDLOCAL
