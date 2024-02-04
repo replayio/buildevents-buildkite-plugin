fd1fb62c9ea431548f811fbc77e8781e5ea8d2e6
Chris Toshok <ctoshok@replay.io>
2024-02-04 08:45:53 -0700

    dogscience

--- bin/fetch_exe.bat
+++ bin/fetch_exe.bat
@@ -1,5 +1,5 @@
 @echo off
-SETLOCAL
+SETLOCAL enabledelayedexpansion
 
 SET "BUILDEVENTS_GH_ORG=replayio"
 SET "BUILDEVENTS_RELEASE=toshok^%2Fadd-buildkite-watch-v0.3"
@@ -11,8 +11,8 @@ SET "PLUGIN_BIN=%~dp0"
 
 :: Download buildevents if not exists
 IF NOT EXIST %PLUGIN_BIN%buildevents.exe (
-    echo Downloading buildevents from url: https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/%BUILDEVENTS_RELEASE%/buildevents-windows-%ARCH%.exe
-    curl -Lf -o %PLUGIN_BIN%buildevents.exe https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/%BUILDEVENTS_RELEASE%/buildevents-windows-%ARCH%.exe
+    echo Downloading buildevents from url: https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/!BUILDEVENTS_RELEASE!/buildevents-windows-%ARCH%.exe
+    curl -Lf -o %PLUGIN_BIN%buildevents.exe https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/!BUILDEVENTS_RELEASE!/buildevents-windows-%ARCH%.exe
 )
 
 ENDLOCAL
