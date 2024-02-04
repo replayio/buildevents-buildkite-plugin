6cef6e56936371e14be88340ec8a04aa8aee2aa0
Chris Toshok <ctoshok@replay.io>
2024-02-04 08:26:36 -0700

    escape the %

--- bin/fetch_exe.bat
+++ bin/fetch_exe.bat
@@ -2,7 +2,7 @@
 SETLOCAL
 
 SET "BUILDEVENTS_GH_ORG=replayio"
-SET "BUILDEVENTS_RELEASE=toshok%2Fadd-buildkite-watch-v0.3"
+SET "BUILDEVENTS_RELEASE=toshok^%2Fadd-buildkite-watch-v0.3"
 
 :: For now, assume amd64.  is there a way to detect this?
 SET "ARCH=amd64"
