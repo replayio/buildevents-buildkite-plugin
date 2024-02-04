2f85bf471b7bc90948f644e7714a5589259718db
Chris Toshok <ctoshok@replay.io>
2024-02-04 11:18:56 -0700

    switch to buildevents 0.4 to get the error printed

--- bin/fetch_exe.bat
+++ bin/fetch_exe.bat
@@ -2,7 +2,7 @@
 SETLOCAL
 
 SET "BUILDEVENTS_GH_ORG=replayio"
-SET "BUILDEVENTS_RELEASE=toshok/add-buildkite-watch-v0.3"
+SET "BUILDEVENTS_RELEASE=toshok/add-buildkite-watch-v0.4"
 
 :: For now, assume amd64.  is there a way to detect this?
 SET "ARCH=amd64"
