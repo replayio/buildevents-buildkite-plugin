ee46da18e78a35ed1d77b95da422e1b0158679be
Chris Toshok <ctoshok@replay.io>
2024-02-04 09:03:52 -0700

    tableflip

--- bin/fetch_exe.bat
+++ bin/fetch_exe.bat
@@ -11,8 +11,10 @@ SET "PLUGIN_BIN=%~dp0"
 
 :: Download buildevents if not exists
 IF NOT EXIST %PLUGIN_BIN%buildevents.exe (
-    echo Downloading buildevents from url: https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/%BUILDEVENTS_RELEASE%/buildevents-windows-%ARCH%.exe
-    curl -Lf -o %PLUGIN_BIN%buildevents.exe https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/%BUILDEVENTS_RELEASE%/buildevents-windows-%ARCH%.exe
+::    echo Downloading buildevents from url: https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/%BUILDEVENTS_RELEASE%/buildevents-windows-%ARCH%.exe
+::    curl -Lf -o %PLUGIN_BIN%buildevents.exe https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/%BUILDEVENTS_RELEASE%/buildevents-windows-%ARCH%.exe
+    echo Downloading buildevents from url: https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/toshok^%2Fadd-buildkite-watch-v0.3/buildevents-windows-%ARCH%.exe
+    curl -Lf -o %PLUGIN_BIN%buildevents.exe https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/toshok^%2Fadd-buildkite-watch-v0.3/buildevents-windows-%ARCH%.exe
 )
 
 ENDLOCAL
