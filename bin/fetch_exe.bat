0c2adb230034e3bc1443b30e6b18822f15335080
Chris Toshok <ctoshok@replay.io>
2024-02-24 07:40:30 -0800

    More windows testing work (#1)
    bring in a v0.4 of the buildevents executable and add more debugging to see if I can get windows working.

--- bin/fetch_exe.bat
+++ bin/fetch_exe.bat
@@ -2,16 +2,17 @@
 SETLOCAL
 
 SET "BUILDEVENTS_GH_ORG=replayio"
-SET "BUILDEVENTS_RELEASE=toshok%2Fadd-buildkite-watch-v0.3"
+SET "BUILDEVENTS_RELEASE=toshok/add-buildkite-watch-v0.4"
 
-:: For not, assume amd64.  is there a way to detect this?
+:: For now, assume amd64.  is there a way to detect this?
 SET "ARCH=amd64"
 
 SET "PLUGIN_BIN=%~dp0"
 
 :: Download buildevents if not exists
-IF NOT EXIST "%PLUGIN_BIN%buildevents.exe" (
-    curl -Lf -o "%PLUGIN_BIN%buildevents.exe" "https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/%BUILDEVENTS_RELEASE%/buildevents-windows-%ARCH%.exe"
+IF NOT EXIST %PLUGIN_BIN%buildevents.exe (
+    echo Downloading buildevents from url: https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/%BUILDEVENTS_RELEASE%/buildevents-windows-%ARCH%.exe
+    curl -Lf -o %PLUGIN_BIN%buildevents.exe https://github.com/%BUILDEVENTS_GH_ORG%/buildevents/releases/download/%BUILDEVENTS_RELEASE%/buildevents-windows-%ARCH%.exe
 )
 
 ENDLOCAL
--- hooks/pre-command.bat
+++ hooks/pre-command.bat
@@ -6,7 +6,7 @@ SET "PLUGIN_ROOT=%~dp0\.."
 SET "PLUGIN_BIN=%PLUGIN_ROOT%\bin"
 
 REM Export _BUILDEVENT_STEP_START_TIME with the current Unix timestamp
-FOR /F "delims=" %%i IN ('powershell -Command "Get-Date -UFormat %%s"') DO SET "_BUILDEVENT_STEP_START_TIME=%%i"
+for /f %%x in ('powershell -command "(Get-Date -UFormat '%%s') -replace '\..*'"') do set "_BUILDEVENT_STEP_START_TIME=%%x"
 
 REM Make sure we have a buildevents binary
 CALL "%PLUGIN_BIN%\fetch_exe.bat"
